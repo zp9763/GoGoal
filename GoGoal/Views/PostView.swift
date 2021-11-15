c8326e56ed90a198915dc3b7d2598d7170ee3b7d
coderballer <shhchen122@gmail.com>
2021-11-15 03:13:21 -0500

    organize ui components in PostView

--- GoGoal/Views/PostView.swift
+++ GoGoal/Views/PostView.swift
@@ -30,68 +30,39 @@ struct PostView: View {
       
       if let owner = self.owner {
         HStack {
-          Spacer()
           
           Image.fromUIImage(uiImage: owner.avatar)?
             .resizable()
             .scaledToFit()
             .clipShape(Circle())
+            .frame(width: 18, height: 18, alignment: .topLeading)
+          
+          Text(owner.getFullName())
+            .foregroundColor(Color.gray)
+            .multilineTextAlignment(.leading)
+            .font(.system(size: 12))
+          
+          Spacer()
+          
+          self.topicIcon?
+            .resizable()
+            .scaledToFit()
+            .clipShape(Rectangle())
             .overlay(
-              Circle()
+              Rectangle()
                 .stroke(Color.white, lineWidth: 2)
                 .shadow(radius: 40)
             )
-            .frame(width: 60, height: 60)
-          
-          Text(owner.getFullName())
-            .bold()
-          
-          Spacer()
-          
-          if let _ = self.post.likes?[self.user.id!] {
-            Button(action: {
-              self.postService.removeUserLike(postId: self.post.id!, userId: self.user.id!)
-              self.post.likes!.removeValue(forKey: self.user.id!)
-              self.postIsLiked = false
-            }) {
-              Image(systemName: "hand.thumbsup.fill")
-            }
-          } else {
-            Button(action: {
-              self.postService.addUserLike(postId: self.post.id!, userId: self.user.id!)
-              if self.post.likes == nil {
-                self.post.likes = [String: Timestamp]()
-              }
-              self.post.likes![self.user.id!] = Timestamp.init()
-              self.postIsLiked = true
-            }) {
-              Image(systemName: "hand.thumbsup")
-            }
-          }
-          
-          Text(String(self.post.likes?.count ?? 0))
-          
-          Spacer()
+            .frame(width: 25, height: 25)
         }
       }
-      
-      HStack {
-        Spacer()
-        
-        self.topicIcon?
-          .resizable()
-          .scaledToFit()
-          .clipShape(Rectangle())
-          .overlay(
-            Rectangle()
-              .stroke(Color.white, lineWidth: 2)
-              .shadow(radius: 40)
-          )
-          .frame(width: 60, height: 60)
-        
-        Spacer()
-        
+
+      HStack{
         Text(self.post.content)
+          .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
+          .foregroundColor(Color.black)
+          .font(.system(size: 8))
+          .padding(.leading, 18.0)
         
         Spacer()
       }
@@ -111,6 +82,38 @@ struct PostView: View {
               .frame(width: 100, height: 80)
           }
         }
+      }
+      
+      HStack {
+        Spacer()
+        if let _ = self.post.likes?[self.user.id!] {
+          Button(action: {
+            self.postService.removeUserLike(postId: self.post.id!, userId: self.user.id!)
+            self.post.likes!.removeValue(forKey: self.user.id!)
+            self.postIsLiked = false
+          }) {
+            Image(systemName: "hand.thumbsup.fill")
+              .resizable()
+              .frame(width: 12, height: 12)
+          }
+        } else {
+          Button(action: {
+            self.postService.addUserLike(postId: self.post.id!, userId: self.user.id!)
+            if self.post.likes == nil {
+              self.post.likes = [String: Timestamp]()
+            }
+            self.post.likes![self.user.id!] = Timestamp.init()
+            self.postIsLiked = true
+          }) {
+            Image(systemName: "hand.thumbsup")
+              .resizable()
+              .frame(width: 12, height: 12)
+          }
+        }
+        
+        Text(String(self.post.likes?.count ?? 0))
+          .font(.system(size: 12))
+          .padding(.trailing, 8)
       }
       
       Spacer()
