b9ba42ec7a2c9478510c5f7639e1d3124edebed7
zp9763 <zp9763@gmail.com>
2021-11-19 02:23:36 -0500

    add a workaround to use dev env for unit tests

--- GoGoal/Utils/EnvironmentConfig.swift
+++ GoGoal/Utils/EnvironmentConfig.swift
@@ -10,6 +10,11 @@ import Foundation
 class EnvironmentConfig {
   
   static func getEnv() -> String {
+    // a workaround to force using dev env for unit tests
+    if let _ = NSClassFromString("XCTest") {
+      return "DEV"
+    }
+    
     // use dev env by default if no env variable is found in plist
     return Bundle.main.object(forInfoDictionaryKey: "Environment") as? String ?? "DEV"
   }
