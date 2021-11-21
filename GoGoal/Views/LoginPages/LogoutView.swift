e4c533a8e49c2a396b1be288499d1b496574ed82
coderballer <shhchen122@gmail.com>
2021-11-20 22:04:39 -0500

    modify placeholder code and improve coding conventions

--- GoGoal/Views/LoginPages/LogoutView.swift
+++ GoGoal/Views/LoginPages/LogoutView.swift
@@ -14,56 +14,50 @@ struct LogoutView: View {
   
   var body: some View {
     ZStack {
-      Color.blue.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-      
-      // TODO: disable environment tag when releasing app
-//      Group {
-//        Text("Environment: \(EnvironmentConfig.getEnv())")
-//        Text("(toggled on for app developers)")
-//        Spacer()
-//      }
+      Color.blue.edgesIgnoringSafeArea(.all)
       
       VStack{
         Spacer()
+        
         Text("Go!Goal!").font(.largeTitle).bold().foregroundColor(.white)
+        
         Spacer()
-        Image("starting_page_image").frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        
+        Image("starting_page_image").frame(width: 100, height: 100, alignment: .center)
+        
         Spacer()
-      Button(action: {
-        self.showLoginView = true
-      }) {
-        Text("Login")
-          .bold()
-          .foregroundColor(.white)
-      }
-      .frame(width: 250, height: 15)
-      .padding()
-      .background(Color.black)
-      .clipShape(Capsule())
-      
         
         Button(action: {
-        self.showSignUpView = true
-      }) {
-        Text("Sign Up")
-          .bold()
-          .foregroundColor(.black)
-      }
-      .frame(width: 250, height: 15)
-      .padding()
-      .background(Color.white)
-      .clipShape(Capsule())
-      Spacer()
-      
+          self.showLoginView = true
+        }) {
+          Text("Login")
+            .bold()
+            .foregroundColor(.white)
+        }
+        .frame(width: 250, height: 15)
+        .padding()
+        .background(Color.black)
+        .clipShape(Capsule())
+        
+        Button(action: {
+          self.showSignUpView = true
+        }) {
+          Text("Sign Up")
+            .bold()
+            .foregroundColor(.black)
+        }
+        .frame(width: 250, height: 15)
+        .padding()
+        .background(Color.white)
+        .clipShape(Capsule())
+        
+        Spacer()
         
       }.alignmentGuide(.bottom, computeValue: { dimension in
         .leastNonzeroMagnitude
-      })
-
-      
+        })
 
     }
-    //.background(Color(.purple).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
     .sheet(isPresented: self.$showLoginView) { LoginView() }
     .sheet(isPresented: self.$showSignUpView) { SignUpView() }
   }
