11ee3e6f0f2d9506a9d4d9084be0a2c6f1783970
coderballer <shhchen122@gmail.com>
2021-11-27 06:05:55 -0500

    1: improve community page view(picture grid needs to be further organized later); 2: fix probelm for goal page when switching brightness mode

--- GoGoal/Views/MainPages/CommunityView.swift
+++ GoGoal/Views/MainPages/CommunityView.swift
@@ -16,44 +16,51 @@ struct CommunityView: View {
   let postService = PostService()
   
   var body: some View {
+    
     NavigationView {
-      List {
-        ForEach(self.displayedPosts) {
-          PostView(user: self.userViewModel.user, post: $0)
-        }
-      }
-      .navigationBarTitle("Community", displayMode: .inline)
-      .navigationBarItems(
-        leading: Menu(content: {
-          Button(action: {
-            self.displayedPosts = self.displayedPosts
-              .sorted() { $0.createDate > $1.createDate }
-          }) {
-            Text("Sort by Time")
-          }
-          
-          Button(action: {
-            self.displayedPosts = self.displayedPosts
-              .sorted() { $0.topicId < $1.topicId }
-          }) {
-            Text("Sort by Topic")
-          }
-          
-          Button(action: {
-            self.displayedPosts = self.displayedPosts
-              .sorted() { $0.likes?.count ?? 0 > $1.likes?.count ?? 0 }
-          }) {
-            Text("Sort by Likes")
-          }
-        }) {
-          Image(systemName: "equal.circle")
-        },
+      
+      ScrollView {
+        Divider()
         
-        trailing: Button(action: self.fetchRecentPosts) {
-          Image(systemName: "arrow.clockwise")
+        VStack {
+          ForEach(self.displayedPosts) {
+            Divider()
+            PostView(user: self.userViewModel.user, post: $0)
+          }
         }
-      )
-      .onAppear(perform: self.fetchRecentPosts)
+        .navigationBarTitle("Community", displayMode: .inline)
+        .navigationBarItems(
+          leading: Menu(content: {
+            Button(action: {
+              self.displayedPosts = self.displayedPosts
+                .sorted() { $0.createDate > $1.createDate }
+            }) {
+              Text("Sort by Time")
+            }
+            
+            Button(action: {
+              self.displayedPosts = self.displayedPosts
+                .sorted() { $0.topicId < $1.topicId }
+            }) {
+              Text("Sort by Topic")
+            }
+            
+            Button(action: {
+              self.displayedPosts = self.displayedPosts
+                .sorted() { $0.likes?.count ?? 0 > $1.likes?.count ?? 0 }
+            }) {
+              Text("Sort by Likes")
+            }
+          }) {
+            Image(systemName: "equal.circle")
+          },
+          
+          trailing: Button(action: self.fetchRecentPosts) {
+            Image(systemName: "arrow.clockwise")
+          }
+        )
+        .onAppear(perform: self.fetchRecentPosts)
+      }
     }
   }
   
